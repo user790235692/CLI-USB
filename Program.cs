48a7f853d1e6f8967aba98dc550433e5e6ccdc5d
Lucas <43614096+Twistere@users.noreply.github.com>
2020-09-24 11:34:10 +0200

    update: Rework the code

--- Program.cs
+++ Program.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpSts
 {
     public class Program
     {
- 
-       public static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Usb usb = new Usb();
-            usb.Capacity();
+            string valueInput = Console.ReadLine();
+            FetchUSB(Int32.Parse(valueInput));
+        }
+
+        public static List<Usb> CreateUSB() {
+            List<Usb> usb = new List<Usb>();
+            
+            usb.Add(new Usb(4, 1073741824));
+            usb.Add(new Usb(8, 8589934592));
+            usb.Add(new Usb(16, 17179869184));
+            usb.Add(new Usb(32, 34359738368));
+            usb.Add(new Usb(64, 68719476736));
+            usb.Add(new Usb(128, 137438953472));
+            usb.Add(new Usb(256, 274877906944));
+            usb.Add(new Usb(512, 549755813888));
+
+            return usb;
+        }
+
+
+        public static void FetchUSB(int RealCapacityGo)
+        {
+            List<Usb> usb_keys = CreateUSB();
+
+            foreach(var usb in usb_keys)
+            {
+                if (usb.RealCapacityGo == RealCapacityGo)
+                    Console.WriteLine($"USB trouvé : {usb}");
+            }   
         }
     }
 
--- Usb.cs
+++ Usb.cs
@@ -6,33 +6,18 @@ namespace CSharpSts
 {
     public class Usb 
     {
-
         public int RealCapacityGo {get; set;}
 
         public ulong RealCapacityBytes {get; set;}
 
-        public override string ToString()
-        {
-            return "Go: " + RealCapacityGo + "   Byte: " + RealCapacityBytes;
+        public Usb(int RealCapacityGo, ulong RealCapacityBytes) {
+            this.RealCapacityGo = RealCapacityGo;
+            this.RealCapacityBytes = RealCapacityBytes;
         }
 
-        public void Capacity()
+        public override string ToString()
         {
-            List<Usb> usb = new List<Usb>();
-
-            usb.Add(new Usb() {RealCapacityGo=4, RealCapacityBytes=4294967296});
-            usb.Add(new Usb() {RealCapacityGo=8, RealCapacityBytes=8589934592});
-            usb.Add(new Usb() {RealCapacityGo=16, RealCapacityBytes=17179869184});
-            usb.Add(new Usb() {RealCapacityGo=32, RealCapacityBytes=34359738368});
-            usb.Add(new Usb() {RealCapacityGo=64, RealCapacityBytes=68719476736});
-            usb.Add(new Usb() {RealCapacityGo=128, RealCapacityBytes=137438953472});
-            usb.Add(new Usb() {RealCapacityGo=256, RealCapacityBytes=274877906944});
-            usb.Add(new Usb() {RealCapacityGo=512, RealCapacityBytes=549755813888});
-
-            foreach(Usb usb1 in usb)
-            {
-                Console.WriteLine(usb1);
-            }
+            return $"Go: {RealCapacityGo}  Byte: {RealCapacityBytes}";
         }
     }
 
