86ba5ac57ac3e31323e33d33edeb6808733503cd
Lucas <43614096+Twistere@users.noreply.github.com>
2020-09-26 22:20:02 +0200

    feat: ask if the usb is plug

---
+++ IsPlug.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace CSharpSts
+{
+    public class IsPlug
+    {
+        public bool getBoolInputValue(string intputType)
+        {
+           
+            if (intputType.ToLower() == "yes")
+            {
+            return true;
+            }
+            else if (intputType.ToLower() == "no")
+            {
+            return  false;
+            }
+
+            else
+            {
+            return false;
+            }
+
+        }
+    }
+        
+}
